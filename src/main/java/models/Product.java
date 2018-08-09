5f00faaa99e96e19692e3c4037b36a3f3ca16e51
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-08-09 14:12:03 +0700

    add Product.java and Category.java

---
+++ src/main/java/models/Product.java
@@ -0,0 +1,52 @@
+package models;
+
+public class Product {
+    private int id;
+    private String code;
+    private String name;
+    private double price;
+    private int category_id;
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+}
